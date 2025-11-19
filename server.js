9dfdc039f17bdca3af1a25aa8bc20c8351c4b5c6
Kit-git-kittyT <radonjic.danilo2018@gmail.com>
2025-11-19 15:25:40 +0100

    Stocket.IO completed for the back-end

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import logger from "morgan"
+import http from "http"
 
 import testJwtRouter from "./controllers/test-jwt.js"
 import authRouter from "./controllers/auth.js"
 import userRouter from "./controllers/users.js"
-import { Server } from "socket-I.O/socket.io.js"
+import { socketServer } from "./socket-I.O/socket.io.js"
+const app = express();
+const server = http.createServer(app)
 
 const port = 3000 || process.env.PORT
 
 dotenv.config();
 
-const app = express();
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
@@ -26,6 +28,7 @@ app.use(logger("dev"));
 app.use("/test-jwt", testJwtRouter);
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
+socketServer(server);
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
